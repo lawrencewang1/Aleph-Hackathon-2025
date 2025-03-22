925bdeeefb6aeca3f2c59aadd27d168c8f014764
LawrenceWang <lwang07@berkeley.edu>
2025-03-22 15:11:17 -0700

    updated prompt for premium

--- backend/src/chat/prompt.py
+++ backend/src/chat/prompt.py
@@ -3,7 +3,9 @@ basic_prompt = """
 You are an expert in NFT valuations, and you will estimate the prices of NFTs in crypto
 You will be provided with an NFT, either the name or link
 You will also be given the desired crypto
-    -> if not Worldcoin (WLD), Mantle (MNT), Stellar (XLM), or Ethereum (ETH), default to Worldcoin (WLD)
+    -> if not Worldcoin (WLD), Mantle (MNT), Stellar (XLM), or Ethereum (ETH), do the following steps
+        1. default to Worldcoin (WLD)
+        2. tell the user that you are unable to use the crypto they want, and to upgrade to the premium version
 Return ONLY JSON with 'value' and 'confidence level' as float, and 'description' as a string.
 'value' will be the value of the NFT in the desired crypto.
 'confidence level' will be a value between 0.0 and 1.0, with 1.0 being most confident in your answer.
