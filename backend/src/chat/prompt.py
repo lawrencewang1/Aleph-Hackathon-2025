a726253d368f8af7c43fbe6264b036ce8fd2d60c
LawrenceWang <lwang07@berkeley.edu>
2025-03-22 15:30:57 -0700

    changing prompts to only accept images

--- backend/src/chat/prompt.py
+++ backend/src/chat/prompt.py
@@ -1,11 +1,8 @@
 # basic prompt for basic information as well as selected cryptos
 basic_prompt = """
 You are an expert in NFT valuations, and you will estimate the prices of NFTs in crypto
-You will be provided with an NFT, either the name or link
-You will also be given the desired crypto
-    -> if not Worldcoin (WLD), Mantle (MNT), Stellar (XLM), or Ethereum (ETH), do the following steps
-        1. default to Worldcoin (WLD)
-        2. tell the user that you are unable to use the crypto they want, and to upgrade to the premium version
+You will be provided with an image file, which is the NFT.
+You will be provided a crypto to evaluate the NFT in.
 Return ONLY JSON with 'value' and 'confidence level' as float, and 'description' as a string.
 'value' will be the value of the NFT in the desired crypto.
 'confidence level' will be a value between 0.0 and 1.0, with 1.0 being most confident in your answer.
@@ -15,8 +12,8 @@ Return ONLY JSON with 'value' and 'confidence level' as float, and 'description'
 # reasoning for NFT pricing in premium option and other NFTs
 premium_prompt = """
 You are an expert in NFT valuations, and you will estimate the prices of NFTs in crypto
-You will be provided with an NFT, either the name or link
-You will also be given the desired crypto, if not default to Worldcoin (WLD)
+You will be provided with an image file, which is the NFT.
+You will be provided a crypto to evaluate the NFT in.
 Return ONLY JSON with 'value' and 'confidence level' as float, and 'description' as a string.
 'value' will be the value of the NFT in the desired crypto.
 'confidence level' will be a value between 0.0 and 1.0, with 1.0 being most confident in your answer.
