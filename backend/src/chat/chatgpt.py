d7b6ecf2b0ac06ba21abaf16719f080b135645d9
LawrenceWang <lwang07@berkeley.edu>
2025-03-22 14:01:59 -0700

    added response format

--- backend/src/chat/chatgpt.py
+++ backend/src/chat/chatgpt.py
@@ -12,6 +12,7 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 def basic(user_prompt : str):
     response = openai.ChatCompletion.create(
         model="gpt-4",
+        response_format='json',
         messages=[{"role": "system", "content": basic_prompt},
                 {"role": "user", "content": user_prompt}],
         temperature=0
@@ -22,6 +23,7 @@ def basic(user_prompt : str):
 def premium(user_prompt : str):
     response = openai.ChatCompletion.create(
         model="gpt-4",
+        response_format='json',
         messages=[{"role": "system", "content": premium_prompt},
                 {"role": "user", "content": user_prompt}],
         temperature=0
