0555c0d697dc39d9f5f5422b86b45af4ae478f55
Ava Rouzmehr <avarouzz@berkeley.edu>
2025-03-22 14:17:37 -0700

    removed appraisal, debugging html

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,8 @@ import { VerifyBlock } from "./components/Verify";
 export default function App() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-3">
-      <VerifyBlock />
-      <PayBlock />
+      {/* <VerifyBlock />
+      <PayBlock /> */}
     </main>
   );
 }
